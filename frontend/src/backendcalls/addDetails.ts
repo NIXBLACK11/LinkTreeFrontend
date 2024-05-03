cb3b479f0e2aa81976c56967e5785aa552d237f3
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-05-03 20:42:10 +0530

    visitor and owner

---
+++ frontend/src/backendcalls/addDetails.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from 'axios';
+
+export async function addDetails(userName: string, heading: string, url: string, token: string): Promise<boolean> {
+    try {
+        const response = await axios.post(`http://localhost:8080/${userName}/addDetails`, {
+            name: heading,
+            url: url
+        }, {
+            headers: {
+                Authorization: token
+            }
+        });
+
+        if (response.status === 200) {
+            return true;
+        } else {
+            console.log(`Unexpected response status: ${response.status}`);
+            return false;
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const axiosError: AxiosError = error;
+            console.log(axiosError);
+            if (axiosError.response && axiosError.response.status === 401) {
+                console.log("Unauthorized");
+            } else {
+                console.log("An error occurred during validation");
+            }
+        }
+        return false;
+    }
+}
+
--- frontend/src/components/AddButton.tsx
+++ frontend/src/components/AddButton.tsx
@@ -1,8 +1,18 @@
+import { useParams } from "react-router-dom";
+import { addDetails } from "../backendcalls/addDetails"
+import { getCookie } from "../utils/saveCookie";
+
 export const AddButton = () => {
+    const { userName } = useParams<{ userName: string }>();
+    const token = getCookie("jwtToken");
     return (
         <div className="mt-6">
             <button 
-                // onClick={}
+                onClick={() => {
+                    if(userName && token) {
+                        addDetails(userName, "test", "test.com", token)
+                    }
+                }}
                 className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
             >
                 Add Link
