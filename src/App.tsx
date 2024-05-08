ba08c3e375a30ef3658ac0f623836fc121bf515a
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-05-08 19:54:59 +0530

    frontend complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
 import { User } from "./pages/User"
+import { Test } from "./pages/Test"
 // import { NotFound } from "./pages/NotFound"
 
 
@@ -11,6 +12,7 @@ function App() {
     <>
         <BrowserRouter>
           <Routes>
+            <Route path='/test' element={<Test/>}/>
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/' element={<Signin/>}/>
             <Route path='/:userName' element={<User/>}/>
